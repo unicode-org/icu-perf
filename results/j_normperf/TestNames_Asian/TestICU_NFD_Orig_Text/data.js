2ca74831dd4ca2dd6a26f5b10648cfffe0bc85a7
markusicu <markusicu@users.noreply.github.com>
2024-12-09 18:44:07 +0000

    deploy: unicode-org/icu@791a052f8ef96805674d2558dcbc8f749f98dc71

--- results/j_normperf/TestNames_Asian/TestICU_NFD_Orig_Text/data.js
+++ results/j_normperf/TestNames_Asian/TestICU_NFD_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733532252787,
+  "lastUpdate": 1733767488974,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -29337,6 +29337,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 12.132572877755939,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "committer": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "distinct": true,
+          "id": "62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3",
+          "message": "ICU-22908 MF2: Update spec tests and update implementation for recent spec changes\n\nUpdating the spec tests requires two implementation changes:\n* Match on variables rather than expressions --\n  see https://github.com/unicode-org/message-format-wg/pull/877\n* Require attribute values to be literals (if present) --\n  see https://github.com/unicode-org/message-format-wg/pull/894\n\nThis updates the spec tests to commit 6c3704f41a9c24427126429fb43992b03609dfc8\nin https://github.com/unicode-org/message-format-wg/ . Any changes\nfollowing that commit will be addressed in a future PR.",
+          "timestamp": "2024-12-09T09:38:28-08:00",
+          "tree_id": "36407eae7eb7fb7b5ea9cc9f7586abb74077ec9b",
+          "url": "https://github.com/unicode-org/icu/commit/62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3"
+        },
+        "date": 1733766687138,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 11.768917492799734,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "791a052f8ef96805674d2558dcbc8f749f98dc71",
+          "message": "ICU-22921 fix link from gitdev to ci exhaustive tests",
+          "timestamp": "2024-12-09T09:55:54-08:00",
+          "tree_id": "27107866bf117af7485d2f290251ad447190c880",
+          "url": "https://github.com/unicode-org/icu/commit/791a052f8ef96805674d2558dcbc8f749f98dc71"
+        },
+        "date": 1733767291026,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 11.73982243954857,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
